fec64b79b60ec2db5dc36b2b26241716ba5aff53
Marijan Šuflaj <msufflaj32@gmail.com>
2019-02-21 22:37:18 +0100

    Make InterruptException extend Exception directly
    Since InterruptException is ment for hard interrupt,
    and BeanstalkdLibException is expected to be caught
    somewhere in the caller, InterruptException can not
    extend BeanstalkdLibException.
    
    RuntimeException and LogicException are also exceptions
    with semantic attached to them and are likely to be handled.
    
    Thus, extending Exception is only choice.

--- src/InterruptException.php
+++ src/InterruptException.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace Zlikavac32\BeanstalkdLib;
 
+use Exception;
 use Throwable;
 
-class InterruptException extends BeanstalkdLibException {
+class InterruptException extends Exception {
 
     public function __construct(Throwable $previous = null) {
-        parent::__construct('Interrupt received', $previous);
+        parent::__construct('Interrupt received', 0, $previous);
     }
 }
