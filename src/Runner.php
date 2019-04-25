6ad7b359ef409f1be0da10d875544f1ed7dc6b50
Marijan Šuflaj <msufflaj32@gmail.com>
2019-04-25 10:52:29 +0200

    Add method comment about runner responsibility

--- src/Runner.php
+++ src/Runner.php
@@ -7,5 +7,8 @@ namespace Zlikavac32\BeanstalkdLib;
 interface Runner
 {
 
+    /**
+     * Runner is responsible for job deletion, burying, rescheduling etc.
+     */
     public function run(JobHandle $jobHandle): void;
 }
